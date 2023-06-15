49e83b9b14a3be47581d4954c24396ef83c9aba7
Rayane Vieira <rayvieiraaa@gmail.com>
2023-06-15 08:22:24 -0300

    Inicio controle de versão

---
+++ src/main/java/com/itb/inf2gm/comercio/controller/LojaController.java
@@ -0,0 +1,14 @@
+package com.itb.inf2gm.comercio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping ("/comercio/produtos")
+public class LojaController {
+	public void listarProdutos() {
+		System.out.println("Lista de produto");
+	}
+
+	
+}
