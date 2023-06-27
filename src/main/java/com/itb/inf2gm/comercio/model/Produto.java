b3048eb3dddd5f57d50ae54f99c6a3861e04053d
Rayane Vieira <rayvieiraaa@gmail.com>
2023-06-27 08:26:15 -0300

    aula legal

--- src/main/java/com/itb/inf2gm/comercio/model/Produto.java
+++ src/main/java/com/itb/inf2gm/comercio/model/Produto.java
@@ -13,5 +13,49 @@ public class Produto {
 	// da própria classe.
 	// Entende-se por membros (atributos e ou métodos)
 	
+	// metodos stter's e getter são responsáveis em atribuir e recuperar informações
+	// dos atributos
 	
-}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getCodigoBarra() {
+		return codigoBarra;
+	}
+
+	public void setCodigoBarra(String codigoBarra) {
+		this.codigoBarra = codigoBarra;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+
+	}
+	
